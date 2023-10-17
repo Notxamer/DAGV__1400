7c7539d925de4c94ddf12c0c663c77b2fe6a31cf
Notxamer <maxtonleasure@gmail.com>
2023-10-17 13:17:32 -0600

    Collectable Spawner

--- Ufo/Assets/Scripts/Collect.cs
+++ Ufo/Assets/Scripts/Collect.cs
@@ -7,11 +7,16 @@ public class Colect : MonoBehaviour
     public Transform Spawner;
     public GameObject Collect;
 
-  
+    private float spawnRangeX = 20f;
+    private float spawnPositionZ = 20f;
+    private float startDelay = 5f;
+    private float spawnInterval = 1.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomLocation", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -19,9 +24,15 @@ public class Colect : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
-            Instantiate(Collect, Spawner.transform.position, Collect.transform.rotation);
+            
         }
        
+    }
+
+    void SpawnRandomLocation()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPositionZ);
+        Instantiate(Collect, spawnPos, Spawner.transform.rotation); //Spawns Ufo's through index and array from random location on X-Axis
     }
 
 }
