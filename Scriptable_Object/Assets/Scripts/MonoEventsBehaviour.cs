af8e826fcbd85cf3064d31caba66a4751ca81a88
Notxamer <maxtonleasure@gmail.com>
2023-11-02 16:03:53 -0600

    EventsLab

---
+++ Scriptable_Object/Assets/Scripts/MonoEventsBehaviour.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehaivior : MonoBehaviour
+{
+    public UnityEvent startEvent, awakeEvent, disableEvent;
+    private void Awake()
+    {
+        awakeEvent.Invoke();
+    }
+
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        disableEvent.Invoke();
+    }
+}
